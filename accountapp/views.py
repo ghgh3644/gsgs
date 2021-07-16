432902e60909a6a7880b822f7b0ffd6b3f56d14b
Lee Geonhee <gs>
2021-07-16 09:16:12 +0900

    7/16 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -20,9 +20,10 @@ def hello_world(request):
         return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else :
         data_list = NewModel.objects.all()
-        return render(request, 'accountsapp/hello_world.html', context={'data_list': data_list})
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
 
-class AccountCreatView(CreateView):
+
+class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
