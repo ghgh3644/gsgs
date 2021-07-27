363a4a8953681efba508c4a7db4aacd6a22a8e45
Lee Geonhee <gs>
2021-07-27 09:46:36 +0900

    7/27 3ban class commit 2

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,10 @@
+from django.urls import path
 
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileapp'
 
 urlpatterns = [
-
+    path('create/', ProfileCreateView.as_view(), name='create'),     #장고에서 사용하는 것 가져오기
 
 ]
