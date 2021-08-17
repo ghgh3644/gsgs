9751832470add69382c9e11d454a766becbf68a6
Lee Geonhee <gs>
2021-08-17 10:22:44 +0900

    8/17 3ban class commit 2

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -6,5 +6,5 @@ app_name = 'projectapp'
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
-    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail')
+    path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
 ]
