6f7c80f52fc96bd47d475fa8d22e04933a3be900
Lee Geonhee <gs>
2021-08-27 09:22:32 +0900

    8/27 3ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -14,26 +14,11 @@ from accountapp.models import NewModel
 from articleapp.models import Article
 
 
-@login_required
-def hello_world(request):
-    if request.method == 'POST':
-        temp = request.POST.get('input_text')
-
-        model_instance = NewModel()
-        model_instance.text = temp
-        model_instance.save()
-
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else :
-        data_list = NewModel.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
-
-
 
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 class AccountDetailView(DetailView, MultipleObjectMixin):
@@ -56,7 +41,7 @@ class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/update.html'
 
     def get_success_url(self):
@@ -67,6 +52,6 @@ class AccountUpdateView(UpdateView):
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/delete.html'
 
